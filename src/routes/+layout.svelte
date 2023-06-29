4b5cf65117c19b7fa33d2d2afd7ceb8a9fc490bd
Gaëtan H <steellgold0@gmail.com>
2023-06-29 17:37:10 +0200

    chore(package.json): add @vercel/analytics and cookie-muncher as dependencies feat(+layout.svelte): add analytics injection and import dev environment variable

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
   import "../app.css";
+
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
+
 
 <div class="mt-5">
   <slot />
